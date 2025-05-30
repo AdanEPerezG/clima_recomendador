f270ed8bdc7ccb18f36ecada9e5190add40d30c2
Adán Eulalio Pérez <aperezg45@miumg.edu.gt>
2025-05-29 20:36:47 -0600

    Agregar CORS para permitir conexion desde localhost

--- main.py
+++ main.py
@@ -24,3 +24,16 @@ def recomendar_ropa(temp: Temperatura):
     else:
         ropa = "Playera y pantaloneta"
     return {"recomendacion": ropa}
+
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Solo permite tu app local
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
